f960495d91b055bea4f327172bfbd4f49fb5bfdc
Likith7779 <likithn.24.beai@acharya.ac.in>
2025-05-14 12:19:18 +0530

    Create zigzag.py

---
+++ zigzag.py
@@ -0,0 +1,9 @@
+print("Likith raj n,USN:1AY24AI061,SEC:O")
+lines = 9
+max_spaces = lines 
+for i in range(lines):
+    if i <= max_spaces:
+        spaces = i
+    else:
+        spaces = lines - i - 1
+    print(" " * spaces + "*" * 8)
