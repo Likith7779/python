e06ffcb1cc1245c29f6ed447ac244ba157912648
Likith7779 <likithn.24.beai@acharya.ac.in>
2025-06-24 11:21:46 +0530

    Create Debugging Coin Toss.py

---
+++ Debugging Coin Toss.py
@@ -0,0 +1,19 @@
+print("Likith Raj N, USN:1AY24AI061, SEC:O")
+import random
+guess = ''
+while guess not in ('heads', 'tails'):
+    print('Guess the coin toss! Enter heads or tails:')
+    guess = input().lower()
+toss = random.randint(0, 1)  
+toss_str = 'heads' if toss == 1 else 'tails'
+if guess == toss_str:
+    print('You got it!')
+else:
+    print('Nope! Guess again!')
+    guess = ''
+    while guess not in ('heads', 'tails'):
+        guess = input().lower()
+    if guess == toss_str:
+        print('You got it!')
+    else:
+        print('Nope. You are really bad at this game.')
